cecf5ee7596908e7930e79b787ba635371b80ef8
Shawkot <62845541+Shawkot@users.noreply.github.com>
2020-04-11 21:10:42 +0600

    Create char.cpp

---
+++ char.cpp
@@ -0,0 +1,15 @@
+// This program illustrate the type casting and character output
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    char cvalue = 55;
+    cout << cvalue << endl; // Here the output will show the charcarter 7
+                            // because character 7 is equvalent to number 55 in decimal
+    char cvalue1 = 't';
+    cout << (int)cvalue1 << endl;// (int)cvalue1 does the type casting
+
+}
+
