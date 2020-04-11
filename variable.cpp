3a96f25bb26e7507b293232054dc5b35693934cd
Shawkot <62845541+Shawkot@users.noreply.github.com>
2020-04-11 14:29:28 +0600

    Create variable.cpp

---
+++ variable.cpp
@@ -0,0 +1,15 @@
+// This program use variable to output something
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int numberDogs = 8;
+    int numberCats = 7;
+
+    cout << "Number of dog is:" << numberDogs <<endl;
+    cout << "Number of cats is:" << numberCats << endl;
+    cout << "Total number of animals:"<< numberDogs + numberCats << endl;
+    return 0;
+}
