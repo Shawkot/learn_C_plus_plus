c69346f11119755634185456b1d03370106011ee
Shawkot <62845541+Shawkot@users.noreply.github.com>
2020-04-11 14:41:59 +0600

    Create string.cpp

---
+++ string.cpp
@@ -0,0 +1,12 @@
+// This program will concatenate two string
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    string text1 = "Shawkot ";
+    string text2 = "Hossain";
+    string text3 = text1 + text2;
+    cout << "Full name:" << text3 <<endl;
+    return 0;
+}
