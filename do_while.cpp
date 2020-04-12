cb8bdceec92da8cf1b1f6da5102f7276b81d78ad
Shawkot <62845541+Shawkot@users.noreply.github.com>
2020-04-12 16:33:28 +0600

    Create do_while.cpp

---
+++ do_while.cpp
@@ -0,0 +1,21 @@
+// This program will infinitely asked password from user
+// untill it finds the correct password
+
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+    const string password = "hello";
+    string input;
+    do{
+    cout << "enter your password > " << flush;
+    cin >> input;
+    if(input != password){
+        cout << "Access Denied " << endl;
+    }
+    }while(input != password);
+
+    cout << "password accepted "<< endl;
+    return 0;
+}
