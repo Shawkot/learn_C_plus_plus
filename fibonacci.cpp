bc062f206a9c59745564f2e17783758327367750
Shawkot <62845541+Shawkot@users.noreply.github.com>
2020-04-20 14:40:47 +0600

    Create fibonacci.cpp

---
+++ fibonacci.cpp
@@ -0,0 +1,40 @@
+// This program will compute and show the nth fibonacci number of the series
+
+#include <iostream>
+#include <vector>
+#include <cassert>
+using namespace std;
+
+/*int fibonacci_naive(int n) {
+    if (n <= 1)
+        return n;
+    else{
+    return fibonacci_naive(n - 1) + fibonacci_naive(n - 2);
+    }
+}*/
+int fibonacci_fast(int n) {
+    int i;
+    int num[n];
+    num[0] = 0;
+    num[1] = 1;
+    for(i=2; i <= n; i++){
+        num[i] = num[i-1] + num[i-2];
+    }
+     return num[n];
+}
+/*void test_solution() {
+    assert(fibonacci_fast(3) == 2);
+    assert(fibonacci_fast(10) == 55);
+    for (int n = 0; n < 20; ++n)
+        assert(fibonacci_fast(n) == fibonacci_naive(n));
+}*/
+
+int main()
+{
+   int n;
+    cin >> n;
+    //cout << fibonacci_naive(n) << endl;
+    //test_solution();
+    cout << fibonacci_fast(n) << '\n';
+    return 0;
+}
